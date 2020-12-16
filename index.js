fe52d2abe50ec0bf9f7add87c2a6c097c8f0799f
sarmadkamran <kamransarmad9@gmail.com>
2020-12-16 14:53:15 +0500

    FInd Grade

---
+++ index.js
@@ -0,0 +1,46 @@
+// var name =prompt('what your name' )
+// if(name ==="muhammadwaseem"){
+//     alert("welcome")
+// }
+// else{(alert("getout"))
+// }
+
+    
+
+    //  var q =78;
+    //  var w = 56;
+    //  var s = 54;
+    //  var d = 65;
+    //  if(q+w +++ s-d){
+    //     alert("ok")    
+    //  }  
+    //  else{
+    //      alert("get out")
+    //  } 
+
+
+    var percent= +prompt("center your percent")
+    if(percent>=80 && percent<=100){
+        alert("a+")
+    }
+    else if(percent>=70 && percent<=80){
+        alert("a")
+    }
+    else if(percent>=60 && percent<=70){
+        alert("b")
+    }
+    else if(percent>=60 && percent<=70){
+        alert("c")
+    }
+    else if(percent>=50 && percent<=60){
+        alert("d")
+    }
+    else if(percent>=40 && percent<=50){
+        alert("f")
+    }
+    else if(percent>=33 && percent<=33){
+        alert("get out ")
+    }
+else{
+    alert("your are didnot wite correct percent")
+}
